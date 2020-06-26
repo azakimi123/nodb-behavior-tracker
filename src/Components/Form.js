4d6cf195b1a5e85030f9e421516bf034364113cc
azakimi123 <aaron.zakimi@gmail.com>
2020-06-26 12:39:07 -0600

    skeleton first draft

---
+++ src/Components/Form.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: '',
+            place: '',
+            behavior: ''
+        }
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Form;
