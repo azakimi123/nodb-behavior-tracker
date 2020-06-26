5597cac7b9a24bda63bca9de09cdb1585d96819e
azakimi123 <aaron.zakimi@gmail.com>
2020-06-26 12:39:55 -0600

    componentDidMount added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Form from './Components/Form';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      behaviorLog: []
+    }
+  }
+
+
+  componentDidMount() {
+    axios.get('/api/behaviors')
+    .then(res => {
+      this.setState({behaviorLog: res.data})
+    })
+    .catch(err => console.log(err));
+  }
+
+
+
+
+render() {
+  // console.log(this.state.behaviorLog[0])
+  let mappedBehaviors = this.state.behaviorLog.map((behavior, index) => (
+    <Form key={index} behavior={behavior}/>
+  ))
   return (
     <div>
-
+      {mappedBehaviors}
     </div>
-  );
+  )
+}
 }
 
 export default App;
