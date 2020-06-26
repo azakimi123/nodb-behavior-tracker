fa43d49013bff453637245b948689839668b28cc
azakimi123 <aaron.zakimi@gmail.com>
2020-06-26 14:49:29 -0600

    title, profile pic, profile name completed

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+            <h1>BEHAVIOR TRACKER</h1>
+            <img className='profile-pic' src='https://image.flaticon.com/icons/svg/3135/3135715.svg'  alt='profile pic'/>
+            <h5>Name of Person</h5>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
