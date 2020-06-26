40d9b2c9889fdef3d97fe8180a60481fcb0b7eb6
azakimi123 <aaron.zakimi@gmail.com>
2020-06-26 14:49:47 -0600

    imported Header.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Form from './Components/Form';
+// import Form from './Components/Form';
+import Header from './Components/Header';
 import axios from 'axios';
 import './App.css';
 
@@ -11,7 +12,7 @@ class App extends Component {
     }
   }
 
-
+  //display the previously logged behavior
   componentDidMount() {
     axios.get('/api/behaviors')
     .then(res => {
@@ -20,17 +21,45 @@ class App extends Component {
     .catch(err => console.log(err));
   }
 
+  //going to pass this function as a prop to Form.js and update App.js state
+  postBehavior = (info) => {
+    axios.post('/api/behaviors', {info: info})
+    .then(res => {
+      this.setState({behaviorLog: res.data})
+    })
+    .catch(err => console.log(err));
+  }
+
+  //going to pass this as prop to Edit.js and update App.js state
+  editBehavior = (id, newDate, newPlace, newBehavior) => {
+    let date = {date: newDate};
+    let place = {place: newPlace};
+    let behavior = {behavior: newBehavior};
+
+    axios.put(`/api/behaviors/${id}`, date, place, behavior)
+    .then(res => {
+      this.setState({behaviorLog: res.data})
+    })
+    .catch(err => console.log(err));
+  }
+
+  //going to pass this as prop to Edit.js and update App.js state
+  deleteBehavior = (id) => {
+    axios.delete(`/api/behaviors/${id}`)
+    .then(res => {
+      this.setState({behaviorLog: res.data})
+    })
+    .catch(err => console.log(err))
+  }
+
 
 
 
 render() {
   // console.log(this.state.behaviorLog[0])
-  let mappedBehaviors = this.state.behaviorLog.map((behavior, index) => (
-    <Form key={index} behavior={behavior}/>
-  ))
   return (
     <div>
-      {mappedBehaviors}
+      <Header />
     </div>
   )
 }
