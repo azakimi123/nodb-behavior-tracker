9c0f467945dc4c322b923fd1b5f3eaaa7287119d
azakimi123 <aaron.zakimi@gmail.com>
2020-06-27 16:03:56 -0600

    put axios request url fixed

--- src/App.js
+++ src/App.js
@@ -36,16 +36,20 @@ class App extends Component {
     .then(res => {
       this.setState({behaviorLog: res.data})
     })
-    .catch(err => console.log('what are you thinking!'));
+    .catch(err => console.log(err));
   }
 
   //going to pass this as prop to Edit.js and update App.js state
   editBehavior = (id, newDate, newPlace, newBehavior) => {
-    let date = {date: newDate};
-    let place = {place: newPlace};
-    let behavior = {behavior: newBehavior};
+    // let date = {date: newDate};
+    // let place = {place: newPlace};
+    // let behavior = {behavior: newBehavior};
 
-    axios.put(`/api/behaviors/${id}`, date, place, behavior)
+    axios.put(`/api/behavior/${id}`, {
+      date: newDate,
+      place: newPlace,
+      behavior: newBehavior
+    })
     .then(res => {
       this.setState({behaviorLog: res.data})
     })
@@ -64,6 +68,7 @@ class App extends Component {
 
 
 
+
 render() {
   // console.log(this.state.behaviorLog[0])
   return (
@@ -73,7 +78,8 @@ render() {
         postBehaviorFn={this.postBehavior}/>
       <Tracked
         behaviorLog={this.state.behaviorLog}
-        deleteFn={this.deleteBehavior}/>
+        deleteFn={this.deleteBehavior}
+        editFn={this.editBehavior}/>
     </div>
   )
 }
