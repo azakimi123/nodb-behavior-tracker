eeafc4ebcb6d5e41c7a2088cdb3372d9abb24265
azakimi123 <aaron.zakimi@gmail.com>
2020-06-27 11:09:39 -0600

    changed profile pic

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div>
             <header>
             <h1>BEHAVIOR TRACKER</h1>
-            <img className='profile-pic' src='https://image.flaticon.com/icons/svg/3135/3135715.svg'  alt='profile pic'/> {/*https://image.flaticon.com/icons/svg/3135/3135823.svg*/}
+            <img className='profile-pic' src='https://image.flaticon.com/icons/svg/2922/2922518.svg'  alt='profile pic'/> 
             <h5>Name of Person</h5>
             </header>
         </div>
